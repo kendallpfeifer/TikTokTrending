772161eb2b89e40e85a39a15319fe331371135a5
kendallpfeifer <72325900+kendallpfeifer@users.noreply.github.com>
2022-04-28 17:55:47 -0400

    Create page.py

---
+++ page.py
@@ -0,0 +1,10 @@
+## Introduction 
+
+'''If you're an active member of society, you've most likely heard of TikTok, if not post on it and watch these videos. I personally have 
+cousins as young as 9 who are on TikTok, and hae seen how addictive and immersive it has become for millions of poeple. Seeing my little 
+cousins making dance videos and learning that TikTokers are now a brand of right influencers, I wanted to learn more about what really makes 
+these videos go viral. Is there a formula that gets videos seen on the algoritm? Is it a specific type of person? A common hashtag? Having people 
+tagged? What is it that makes TikToks go viral? I found that there are datasets for the top 1000 tranding videos, so I wanted to use data science
+to see if there is something algorithmically that makes these videos "blow up", as the kids on TikTok say. 
+
+For this project, you'll need: '''
